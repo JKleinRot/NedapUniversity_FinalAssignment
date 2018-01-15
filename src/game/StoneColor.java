2a1dd18a4a9db2f706dd531ac241548030270ee2
Janine Klein Rot <janine.kleinrot@nedap.com>
2018-01-15 16:48:28 +0100

    Added enum for the stone color

---
+++ src/game/StoneColor.java
@@ -0,0 +1,12 @@
+package game;
+
+/** 
+ * The color of the stone on the Go board.
+ * The possible colors are WHITE and BLACK.
+ * @author janine.kleinrot
+ */
+public enum StoneColor {
+	
+	WHITE, BLACK;
+
+}
