1a192904930182cae8bc9de4b1236851a6e66257
Janine Klein Rot <janine.kleinrot@nedap.com>
2018-01-15 15:00:57 +0100

    Added provided GUI package

---
+++ src/gui/InvalidCoordinateException.java
@@ -0,0 +1,12 @@
+package gui;
+
+/**
+ * Created by daan.vanbeek on 15-12-16.
+ */
+@SuppressWarnings("serial")
+public class InvalidCoordinateException extends Exception {
+
+	public InvalidCoordinateException(String message) {
+        super(message);
+    }
+}
