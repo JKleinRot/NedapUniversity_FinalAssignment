23706f3dad290796105dd150c191967654b375f4
Janine Klein Rot <janine.kleinrot@nedap.com>
2018-01-16 13:50:47 +0100

    Deleted the GoGUI from the HumanPlayer

--- src/player/HumanPlayer.java
+++ src/player/HumanPlayer.java
@@ -1,6 +1,5 @@
 package player;
 
-import gui.GoGUIIntegrator;
 import stone.StoneColor;
 /**
  * Human player for Go.
@@ -8,21 +7,15 @@ import stone.StoneColor;
  */
 public class HumanPlayer extends Player {
 	
-	/** GUI for the Go board. */
-	private GoGUIIntegrator goGUI;
-	
 	/**
 	 * Creates a human player with a given name and stone color.
-	 * It starts the GUI of the Go board.
 	 * @param name
 	 * 			The name of the human player.
 	 * @param color
 	 * 			The color of the stones of the human player.
 	 */
-	public HumanPlayer(String name, StoneColor color, int boardSize) {
+	public HumanPlayer(String name, StoneColor color) {
 		super(name, color);
-		goGUI = new GoGUIIntegrator(true, true, boardSize);
-		goGUI.startGUI();
 	}
-
+	
 }
