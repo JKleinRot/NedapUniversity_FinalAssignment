d89e6522e507072b192c9ea723770d02cdcad76b
Janine Klein Rot <janine.kleinrot@nedap.com>
2018-01-16 08:41:31 +0100

    Moved Board and Intersection to separate package board and implemented those classes

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,5 +1,80 @@
-package game;
+package board;
 
+import stone.Stone;
+import stone.StoneColor;
+
+/**
+ * The Go board.
+ * @author janine.kleinrot
+ */
 public class Board {
+	
+	/** Two-dimensional array of intersections of the board. */
+	private Intersection[] [] intersections;
+	
+	/** 
+	 * Initialize a Go board with the given width.
+	 * Initially all intersections are unoccupied.
+	 * @param width
+	 * 			The width of the board.
+	 */
+	public Board(int size) {
+		intersections = new Intersection[size - 1] [size - 1];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				intersections[i] [j] = new Intersection();
+			}
+		}
+	}
+	
+	/**
+	 * Return the intersection at the provided x and y coordinate of the board.
+	 * @param x
+	 * 			The x coordinate of the intersection at the board.
+	 * @param y
+	 * 			The y coordinate of the intersection at the board.
+	 * @return
+	 * 			The intersection at the board at the provided coordinates.
+	 */
+	public Intersection getIntersection(int x, int y) {
+		return intersections[x] [y];
+	}
+	
+	/**
+	 * Set a stone with the provided color at the intersection at the provided x and y coordinate of the board.
+	 * @param x
+	 * 			The x coordinate of the intersection at the board.
+	 * @param y
+	 * 			The y coordinate of the intersection at the board.
+	 * @param color
+	 * 			The color of the stone set at the intersection.
+	 */
+	public void setStone(int x, int y, StoneColor color) {
+		this.getIntersection(x, y).setStone(color);
+	}
+	
+	/**
+	 * Get the stone at the intersection at the provided x and y coordinate of the board.
+	 * @param x
+	 * 			The x coordinate of the intersection at the board.
+	 * @param y
+	 * 			The y coordinate of the intersection at the board.
+	 * @return 
+	 * 			The stone at the intersection.
+	 */
+	public Stone getStone(int x, int y) {
+		return this.getIntersection(x, y).getStone();
+	}
+	
+	/**
+	 * Remove the stone at the intersection at the provided x and y coordinate of the board.
+	 * @param x
+	 * 			The x coordinate of the intersection at the board.
+	 * @param y
+	 * 			The y coordinate of the intersection at the board.
+	 */
+	public void removeStone(int x, int y) {
+		this.getIntersection(x, y).removeStone();
+	}
 
 }
--- src/board/Intersection.java
+++ src/board/Intersection.java
@@ -1,5 +1,65 @@
-package game;
+package board;
 
+import stone.Stone;
+import stone.StoneColor;
+
+/**
+ * Intersection at the Go board.
+ * An intersection can be unoccupied or occupied by either a black stone or a white stone.
+ * @author janine.kleinrot
+ *
+ */
 public class Intersection {
+	
+	/** Whether the intersection is occupied. */
+	private boolean isOccupied;
+	
+	/** Stone at the intersection. */
+	private Stone stone;
+	
+	/** 
+	 * Creates a new intersection that is initially unoccupied. 
+	 */
+	public Intersection() {
+		isOccupied = false;
+	}
+	
+	/** 
+	 * Put a stone at the intersection with the provided color. 
+	 * @param color
+	 * 			The color of the stone.
+	 */
+	public void setStone(StoneColor color) {
+		isOccupied = true;
+		stone = new Stone(color);
+	}
+	
+	/**
+	 * Returns the stone at the intersection
+	 * @return
+	 * 			The stone at the intersection.
+	 */
+	public Stone getStone() {
+		return stone;
+	}
+	
+	/**
+	 * Remove stone at the intersection.
+	 */
+	public void removeStone() {
+		isOccupied = false;
+		stone = null;
+	}
+	
+	/**
+	 * Returns if a stone is at the intersection.
+	 * @return
+	 * 			True if a stone occupies the intersection and false otherwise.
+	 */
+	public boolean isOccupied() {
+		return isOccupied;
+	}
+	
+	
 
 }
