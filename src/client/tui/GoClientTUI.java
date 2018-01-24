70879ab0d47a7509caff5f179ea2ca986c8cff6e
Janine Klein Rot <janine.kleinrot@nedap.com>
2018-01-24 08:03:46 +0100

    Changed name + stoneColor of the error messages to ERROR

--- src/client/tui/GoClientTUI.java
+++ src/client/tui/GoClientTUI.java
@@ -170,18 +170,18 @@ public class GoClientTUI implements Observer, Runnable {
 		} else if (object.equals("Valid move")) {
 			goClientActor.sendMove(move);
 		} else if (object.equals("Invalid move input")) {
-			System.out.println(name + stoneColor + ": Not a valid move");
+			System.out.println("ERROR: Not a valid move");
 			System.out.println(name + stoneColor + ": Waiting on move...");
 		} else if (object.equals("Move not on board")) {
-			System.out.println(name + stoneColor + ": Not a valid move "
+			System.out.println("ERROR: Not a valid move "
 					+ "(Intersection out of range of board)");
 			System.out.println(name + stoneColor + ": Waiting on move...");
 		} else if (object.equals("Occupied intersection")) {
-			System.out.println(name + stoneColor + ": Not a valid move "
+			System.out.println("ERROR: Not a valid move "
 					+ "(Intersection already occupied with stone)");
 			System.out.println(name + stoneColor + ": Waiting on move...");
 		} else if (object.equals("Ko rule")) {
-			System.out.println(name + stoneColor + ": Not a valid move (Ko rule violated");
+			System.out.println("ERROR: Not a valid move (Ko rule violated");
 			System.out.println(name + stoneColor + ": Waiting on move...");
 		}
 		
